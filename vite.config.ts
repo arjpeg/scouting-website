00b8c741e03546e4a134f6f3108a4e774e7bc5ef
arjpeg <58893337+arjpeg@users.noreply.github.com>
2026-01-28 13:15:57 -0600

    add deployment with firebase

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  server: {
-    hmr: false, // disable Vite HMR/auto-reload; manual refresh only
-  },
+  build: {
+    outDir: '../dist'
+  }
 })
